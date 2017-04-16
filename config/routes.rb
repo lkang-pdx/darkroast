70e1e989fa44e013870586654fb81fd3c8ef5a14
Luna Kang <luna.kang.ca@gmail.com>
2017-04-16 14:29:24 -0700

    API Version

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :todos do
-    resources :items
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :todos do
+      resources :items
+    end
   end
+  
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 end
